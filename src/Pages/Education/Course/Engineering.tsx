8d44ea108aa441039464e5ba4318e32f60805cc4
John renz <johnrenzbandianon9@gmail.com>
2025-11-30 13:24:35 +0800

    Remove unused React imports to fix TS6133 build error

--- src/Pages/Education/Course/Engineering.tsx
+++ src/Pages/Education/Course/Engineering.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { Link, useLocation } from "react-router-dom";
 import DashboardHeader from "../../Dashboard/DashboardHeader";
